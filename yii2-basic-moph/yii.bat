e014862aa78bf3d446223fbf0b698cae39da0bd7
OhMmOtTA <ohmmotta@OhMmOtTAs-MacBook-Pro.local>
2015-03-12 15:18:02 +0700

    test

--- yii2-basic-moph/yii.bat
+++ yii2-basic-moph/yii.bat
@@ -3,7 +3,7 @@
 rem -------------------------------------------------------------
 rem  Yii command line bootstrap script for Windows.
 rem
-rem  @author Qiang Xue <qiang.xue@gmail.com>
+rem  @author Qiang Xue <qiang.xue@gmaddsdsdsdsil.com>
 rem  @link http://www.yiiframework.com/
 rem  @copyright Copyright (c) 2008 Yii Software LLC
 rem  @license http://www.yiiframework.com/license/
